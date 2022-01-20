9b46d70a7e9c3f8244aeda71b0ec22d72b1b486a
EAbimelec <abimelec70.arenas@gmail.com>
2022-01-20 17:49:27 -0600

    Convert the statusBar function to a class

--- src/components/PersonalStats.js
+++ src/components/PersonalStats.js
@@ -5,37 +5,16 @@ import styles from "./PersonalStats.module.css";
 function PersonalStats(props) {
 
   const personalStats = styles["personal-stats"];
-  const needStates = [10, 50, 35, 25, 30, 2];
   const statisticTypes = {
-    physicalActivity: {
-      label:"PhysicalActivity",
-      colorBar:"#FF5403",
-      needState: `${needStates[0]}%`
-    },
-    socialNeeds: {
-      label:"Social Needs",
-      colorBar:"#FFE162",
-      needState: `${needStates[1]}%`
-    },
-    hygiene: {
-      label:"Hygiene",
-      colorBar:"#F5F5F5",
-      needState: `${needStates[2]}%`
-    },
-    sleep: {
-      label:"Sleep",
-      colorBar:"#683aad",
-      needState: `${needStates[3]}%`
-    },
-    hedonism: {
-      label:"Hedonism",
+    physicalHealth: {
+      label:"Physical Health",
       colorBar:"#A13333",
-      needState: `${needStates[4]}%`
+      needState: `50%`
     },
-    thrist: {
-      label:"Thrist",
+    mentalHealth: {
+      label:"Mental Health",
       colorBar:"#4FBDBA",
-      needState: `${needStates[5]}%`
+      needState: `20%`
     }
   };
 
@@ -45,12 +24,8 @@ function PersonalStats(props) {
         {props.characterName}
       </h1>
       
-      <StateBar {...statisticTypes.physicalActivity}/> 
-      <StateBar {...statisticTypes.socialNeeds}/> 
-      <StateBar {...statisticTypes.hygiene}/> 
-      <StateBar {...statisticTypes.sleep}/>
-      <StateBar {...statisticTypes.hedonism}/>
-      <StateBar {...statisticTypes.thrist}/> 
+      <StateBar {...statisticTypes.physicalHealth}/> 
+      <StateBar {...statisticTypes.mentalHealth}/> 
     </div>
   );
 }
--- src/components/StateBar.js
+++ src/components/StateBar.js
@@ -1,21 +1,27 @@
 import React from "react";
 import styles from "./StateBar.module.css";
 
-function StateBar(props) {
-  
-  const barContainer = styles["bar-container"];
-  const barState = {
-    "--color": props.colorBar,
-    "--need-state": props.needState
+class StateBar extends React.Component {
+
+  // barContainer = styles["bar-container"];
+
+  constructor(props){
+    super(props);
+    this.barContainer = styles["bar-container"];
   }
-  console.log(props.needState);
-  return(
-    <div style={barState} className={barContainer}>
-      <p>{props.label}</p>
-      <div className={styles["state-bar"]}></div>
-    </div>
-    
-  );
+
+  render() {
+    this.barState = {
+      "--color": this.props.colorBar,
+      "--need-state": this.props.needState
+    };
+    return(
+      <div style={this.barState} className={this.barContainer}>
+        <p>{this.props.label}</p>
+        <div className={styles["state-bar"]}></div>
+      </div>
+    );
+  }
 
 }
 
